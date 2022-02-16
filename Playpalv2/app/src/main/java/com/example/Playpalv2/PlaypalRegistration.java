a850c4298041428a85bb4c76a9bf821d992d9c67
MarkRr15 <90010457+MarkRr15@users.noreply.github.com>
2022-02-15 19:05:02 -0800

    Registration added

---
+++ Playpalv2/app/src/main/java/com/example/Playpalv2/PlaypalRegistration.java
@@ -0,0 +1,14 @@
+package com.example.Playpalv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlaypalRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playpal_registration);
+    }
+}
