fddc2d10ca58664de4f6040f0cb01e10e712fe49
Francisco G <98850491+FranciscoGomez1@users.noreply.github.com>
2022-02-15 22:30:20 -0800

    Made the start up activity the register activity. Also added a bottom with onclick listener to navigate to the main activity.

--- Playpalv2/app/src/main/java/com/example/Playpalv2/PlaypalRegistration.java
+++ Playpalv2/app/src/main/java/com/example/Playpalv2/PlaypalRegistration.java
@@ -2,13 +2,27 @@ package com.example.Playpalv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlaypalRegistration extends AppCompatActivity {
+
+   Button btnRegister; //Sign up button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playpal_registration);
+
+        btnRegister = findViewById(R.id.btn_register); // Link to the xml bottom element
+
+       btnRegister.setOnClickListener(new View.OnClickListener() { // Set a clickListner
+           @Override
+           public void onClick(View view) { // When clicked it takes user to the main activity
+               startActivity(new Intent( PlaypalRegistration.this, MainActivity.class));
+           }
+       });
     }
 }
