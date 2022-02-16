5443ebbf7d1681ee4926e41f6da0201974caddff
Agustin Valadez <Agustinavala58@gmail.com>
2022-02-15 17:08:30 -0800

    Testing UI Design for Messages

---
+++ Playpalv2/app/src/main/java/com/example/Playpalv2/NewMatches.java
@@ -0,0 +1,18 @@
+package com.example.Playpalv2;
+
+public class NewMatches {
+    private String title, description;
+
+    public NewMatches(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ Playpalv2/app/src/main/java/com/example/Playpalv2/NewMatchesAdapter.java
@@ -0,0 +1,54 @@
+package com.example.Playpalv2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NewMatchesAdapter extends RecyclerView.Adapter<NewMatchesAdapter.NewMatchesViewHolder> {
+
+    private List<NewMatches> newMatches;
+
+    NewMatchesAdapter(List<NewMatches> newMatches) {
+        this.newMatches = newMatches;
+    }
+
+    @NonNull
+    @Override
+    public NewMatchesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NewMatchesViewHolder(
+            LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.match_container,
+                    parent,
+                    false
+            )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewMatchesViewHolder holder, int position) {
+        holder.textTitle.setText(newMatches.get(position).getTitle());
+        holder.textDescription.setText(newMatches.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return newMatches.size();
+    }
+
+    class NewMatchesViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView textTitle, textDescription;
+
+        NewMatchesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textDescription = itemView.findViewById(R.id.textDescription);
+        }
+    }
+}
