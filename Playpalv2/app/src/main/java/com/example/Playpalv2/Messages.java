3dcd491615e57da30ea31316a30faedbc8a1f750
Agustin Valadez <Agustinavala58@gmail.com>
2022-02-21 13:17:51 -0800

    Updated UI to include time, name, and text message

--- Playpalv2/app/src/main/java/com/example/Playpalv2/Messages.java
+++ Playpalv2/app/src/main/java/com/example/Playpalv2/Messages.java
@@ -2,6 +2,7 @@ package com.example.Playpalv2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,9 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Messages extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;//FOR NAVIGATION BAR
@@ -17,6 +21,21 @@ public class Messages extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+
+        RecyclerView newRecyclerView = findViewById(R.id.newRecyclerView);
+        List<NewMatches> newMatches = new ArrayList<>();
+
+        for(int i = 0; i < 15; i++){
+            newMatches.add(
+                    new NewMatches(
+                            "Rocky ".concat(String.valueOf(i)),
+                            getResources().getString(R.string.dummy_text)
+                    )
+            );
+        }
+
+        newRecyclerView.setAdapter(new NewMatchesAdapter(newMatches));
+
 
         //FOR NAVIGATION BAR
         bottomNavigationView = findViewById(R.id.bottom_navigation);
