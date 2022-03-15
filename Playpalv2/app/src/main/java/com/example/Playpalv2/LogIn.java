a9e10dec75294cad0bfb86e1eeee620103c098d7
Francisco G <98850491+FranciscoGomez1@users.noreply.github.com>
2022-03-15 13:56:44 -0700

    Basic log in form.

---
+++ Playpalv2/app/src/main/java/com/example/Playpalv2/LogIn.java
@@ -0,0 +1,30 @@
+package com.example.Playpalv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.Playpalv2.flipCards.MainActivity;
+
+public class LogIn extends AppCompatActivity {
+
+    private Button logIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        logIn = findViewById(R.id.login_btn);
+
+        logIn.setOnClickListener(View ->{
+            Intent intent = new Intent(LogIn.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        });
+
+    }
+}
