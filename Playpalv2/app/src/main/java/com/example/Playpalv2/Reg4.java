3fda658acd0ce65158d3b68a9fece2a4b1dc835a
Francisco G <98850491+FranciscoGomez1@users.noreply.github.com>
2022-04-06 12:15:32 -0700

    Basic dog images upload works.

--- Playpalv2/app/src/main/java/com/example/Playpalv2/Reg4.java
+++ Playpalv2/app/src/main/java/com/example/Playpalv2/Reg4.java
@@ -3,6 +3,7 @@ package com.example.Playpalv2;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,6 +16,14 @@ import android.widget.Button;
 import com.example.Playpalv2.databinding.ActivityMainBinding;
 import com.example.Playpalv2.databinding.ActivityReg4Binding;
 import com.example.Playpalv2.flipCards.MainActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class Reg4 extends AppCompatActivity {
 
@@ -30,6 +39,9 @@ public class Reg4 extends AppCompatActivity {
     private Button nextBtn;
 
     ActivityReg4Binding binding;
+
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +51,10 @@ public class Reg4 extends AppCompatActivity {
         nextBtn = findViewById(R.id.btn_next4);
 
         pickBtn = findViewById(R.id.pick_photos_button);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
         pickBtn.setOnClickListener(View -> {
 
 
@@ -89,13 +105,45 @@ public class Reg4 extends AppCompatActivity {
 
                     GridAdapter gridAdapter = new GridAdapter(Reg4.this,dogImages);
                     binding.gridView.setAdapter(gridAdapter);
+
+
                     // Do stuff with each photo/video URI.
                 }
+                uploadImages();
                 Log.e("THANK GOD IT WORKS", dogImages[1].toString());
                 return;
 
 
         }
+    }
+
+    private void uploadImages() {
+        for(Uri i: dogImages){
+            uploadImage(i);
+        }
+    }
+
+    private void uploadImage(Uri file) {
+
+               final String randomKey = UUID.randomUUID().toString();
+                StorageReference dogImages = storageReference.child("images/" + randomKey);
+                dogImages.putFile(file)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                                Log.e("WHAT HAPPEN:", "THANK GOD IT WENT TO FIRESTORE");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("WHAT HAPPEN:", "FIRESTORE");
+
+                            }
+                        });
+         Log.e("IMAGES TO BE UPLOADED", file.toString());
+                //getReference("images/"+fileName);
     }
 
 
