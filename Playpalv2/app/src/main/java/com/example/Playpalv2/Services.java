eab02f95c3f09c4e3914be9952aa91365ae637a0
Agustin Valadez <Agustinavala58@gmail.com>
2022-04-05 18:41:36 -0700

    added basic fragments for services

--- Playpalv2/app/src/main/java/com/example/Playpalv2/Services.java
+++ Playpalv2/app/src/main/java/com/example/Playpalv2/Services.java
@@ -2,14 +2,27 @@ package com.example.Playpalv2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Switch;
+
+import androidx.fragment.app.FragmentManager;
 
 import com.example.Playpalv2.databinding.ActivityServicesBinding;
+import com.example.Playpalv2.flipCards.CardBackFragment;
 import com.example.Playpalv2.flipCards.MainActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Services extends DrawerBase {
 
     ActivityServicesBinding activityServicesBinding;
+    FragmentManager manager;
+    FrameLayout myframeLayout;
+    private  String cont1 = "containerService";
+
+    private  String cont = cont1;
+    boolean showingWalking;
+    private Switch aSwitch;
 
 
     private BottomNavigationView bottomNavigationView;//FOR NAVIGATION BAR
@@ -21,9 +34,17 @@ public class Services extends DrawerBase {
         setContentView(activityServicesBinding.getRoot());
         //set title to top bar
         allocateActivityTitle("Services");
+        View frameLayoutView = findViewById(R.id.containerService);
+        aSwitch = findViewById(R.id.switch1);
         //FOR NAVIGATION BAR
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.services);
+
+        aSwitch.setOnClickListener(View ->{
+            flipCard();
+
+        });
+
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.home) {
@@ -38,5 +59,35 @@ public class Services extends DrawerBase {
         });
 
 
+//on clicklistener
+//fragment mannager
+    }
+    //This functions flips the card with a turning animation
+    void flipCard() {
+        int id = getResources().getIdentifier(cont, "id", getPackageName()); //--> THIS GOT TO CHANGE
+        if (showingWalking) {
+            showingWalking = false;
+            getSupportFragmentManager().popBackStack();
+        } else {
+            showingWalking = true;
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(
+                            R.anim.services_walking,  //Servixes
+                            R.anim.services_sitter) //Walking
+                    .replace(id, new CardBackFragment())
+                    .addToBackStack(null)
+                    .commit();
+        }
+    }
+    public static class FragmentUtils {
+        public static boolean mDisableFragmentAnimations = false;
+    }
+
+    // This function turns the card without flip animation
+    public void clearBackStack() {
+        MainActivity.FragmentUtils.mDisableFragmentAnimations = true;
+        getSupportFragmentManager().popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        MainActivity.FragmentUtils.mDisableFragmentAnimations = false;
     }
 }
