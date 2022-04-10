d72a5befa849f1b0197be0c33875431742243e45
MarkRr15 <90010457+MarkRr15@users.noreply.github.com>
2022-04-09 17:27:20 -0700

    EditDogOwnerProfile white page issue

--- Playpalv2/app/src/main/java/com/example/Playpalv2/DogOwnerProfile.java
+++ Playpalv2/app/src/main/java/com/example/Playpalv2/DogOwnerProfile.java
@@ -4,12 +4,33 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.example.Playpalv2.databinding.ActivityServicesBinding;
+import com.example.Playpalv2.flipCards.CardBackFragment;
+import com.example.Playpalv2.flipCards.MainActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class DogOwnerProfile extends AppCompatActivity {
+
+    ActivityServicesBinding activityServicesBinding;
+    FragmentManager manager;
+    FrameLayout myframeLayout;
+    private  String cont2 = "container_dog_ownerprofile_edit";
+
+    private  String cont = cont2;
+    boolean showingWalking;
+    private Button editProfile2;
+
+    private BottomNavigationView bottomNavigationView; //FOR NAVIGATION BAR
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +38,7 @@ public class DogOwnerProfile extends AppCompatActivity {
         setContentView(R.layout.activity_dog_owner_profile);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        editProfile2 = findViewById(R.id.editProfile2);
         setSupportActionBar(myToolbar);
 
         // Get a support ActionBar corresponding to this toolbar
@@ -25,10 +47,18 @@ public class DogOwnerProfile extends AppCompatActivity {
         // Enable the Up button
         assert ab != null;
         ab.setDisplayHomeAsUpEnabled(true);
-        ab.setTitle(R.string.title_activity_dog_owner_profile);
+       /* ab.setTitle(R.string.title_activity_dog_profile);*/
+        ab.setTitle("Dog Owner Profile");
 
+        editProfile2.setOnClickListener(View -> flipCard()); //
+        //Initialise the main cointainers with the card front fragments
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container_dog_owner_profile_edit, new DisplayDogProfile())
+                    .commit();
 
-
+        }
 
     }
 
@@ -41,4 +71,30 @@ public class DogOwnerProfile extends AppCompatActivity {
         return false;
     }
 
+    void flipCard() {
+        int id = getResources().getIdentifier(cont, "id", getPackageName()); //--> THIS GOT TO CHANGE
+        if (showingWalking) {
+            showingWalking = false;
+            getSupportFragmentManager().popBackStack();
+        } else {
+            showingWalking = true;
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(id, new EditDogOwnerProfile())
+                    .addToBackStack(null)
+                    .commit();
+        }
+    }
+    public static class FragmentUtils {
+        public static boolean mDisableFragmentAnimations = false;
+    }
+
+    // This function turns the card without flip animation
+    public void clearBackStack() {
+        MainActivity.FragmentUtils.mDisableFragmentAnimations = true;
+        getSupportFragmentManager().popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        MainActivity.FragmentUtils.mDisableFragmentAnimations = false;
+    }
+
 }
+
